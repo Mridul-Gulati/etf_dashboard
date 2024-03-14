e478abc4d84fe200487933b9b6437195da878302
mridulgulati18 <mridulgulati18@gmail.com>
2024-03-14 20:16:56 +0530

    Deploy

---
+++ etf.py
@@ -0,0 +1,56 @@
+import yfinance as yf
+import dateutil.relativedelta
+import datetime
+from datetime import timedelta
+import streamlit as st
+import pandas as pd
+import time
+import math
+
+st.title('ETF')
+uploaded_file = st.file_uploader('Upload file', type='xlsx')
+
+def get_stock_data(stock, start, end):
+    try:
+        data = yf.download(stock, start=start, end=end, progress = False)
+        if data.empty:
+            return None
+        return data
+    except Exception as e:
+        return None
+
+if 'button_pressed' not in st.session_state:
+    st.session_state.button_pressed = False
+
+if 'last_analysis_time' not in st.session_state:
+    st.session_state.last_analysis_time = 0
+
+if uploaded_file and st.button('Analyse'):
+    st.session_state.button_pressed = True
+res_place = st.empty()
+while st.session_state.button_pressed:
+    res = pd.DataFrame(columns=['ETF', 'Amount', 'Qty'])
+    current_time = time.time()
+    if current_time - st.session_state.last_analysis_time >= 30:  # 5 minutes in seconds
+        st.session_state.last_analysis_time = current_time
+        if uploaded_file.name.endswith('.xlsx'):
+            df = pd.read_excel(uploaded_file)
+            stocks = df.loc[df['Buy Price'].notna(), 'ETF'].tolist()
+            today = datetime.datetime.today().date()
+            for stock in stocks:
+                data = get_stock_data(f"{stock}.NS", today, today + timedelta(days=1))
+                cmp = data['Close'][-1]
+                buy_price = df.loc[df['ETF'] == stock, 'Buy Price'].values[0]
+                pnl = (cmp-buy_price)/buy_price
+                multi_fac = -1*round(pnl*10,2)
+                variable = round((5000 * multi_fac)/100,2)
+                amount = int(5000 + variable)
+                qty = math.ceil(amount / cmp)
+                # print(stock, cmp, pnl)
+                if pnl <= -0.01:
+                    new_res = pd.DataFrame({'ETF': [stock], 'Amount': [amount], 'Qty': [qty]})
+                    res = pd.concat([res,new_res],ignore_index=True)
+            res_place.text('')
+            res_place.table(res)
+        else:
+            st.warning('Please upload an Excel file')
