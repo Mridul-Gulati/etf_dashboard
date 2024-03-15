7135a7027ffbcecdf8b0473d486caa3a6ee4c73a
mridulgulati18 <mridulgulati18@gmail.com>
2024-03-15 17:08:06 +0530

    Updated Req

--- etf.py
+++ etf.py
@@ -29,9 +29,9 @@ if uploaded_file and st.button('Analyse'):
     st.session_state.button_pressed = True
 res_place = st.empty()
 while st.session_state.button_pressed:
-    res = pd.DataFrame(columns=['ETF', 'Amount', 'Qty'])
+    res = pd.DataFrame(columns=['ETF', 'CMP', 'Amount', 'Qty', 'Down%'])
     current_time = time.time()
-    if current_time - st.session_state.last_analysis_time >= 30:  # 5 minutes in seconds
+    if current_time - st.session_state.last_analysis_time >= 300:
         st.session_state.last_analysis_time = current_time
         if uploaded_file.name.endswith('.xlsx'):
             df = pd.read_excel(uploaded_file)
@@ -39,16 +39,16 @@ while st.session_state.button_pressed:
             today = datetime.datetime.today().date()
             for stock in stocks:
                 data = get_stock_data(f"{stock}.NS", today, today + timedelta(days=1))
-                cmp = data['Close'][-1]
+                cmp = round(data['Close'][-1],2)
                 buy_price = df.loc[df['ETF'] == stock, 'Buy Price'].values[0]
                 pnl = (cmp-buy_price)/buy_price
-                multi_fac = -1*round(pnl*10,2)
-                variable = round((5000 * multi_fac)/100,2)
-                amount = int(5000 + variable)
+                multi_fac = -1*round(pnl*1000,2)
+                variable = round((2500 * multi_fac)/100,2)
+                amount = int(2500 + variable)
                 qty = math.ceil(amount / cmp)
-                # print(stock, cmp, pnl)
-                if pnl <= -0.01:
-                    new_res = pd.DataFrame({'ETF': [stock], 'Amount': [amount], 'Qty': [qty]})
+                print(stock, cmp, pnl, amount, qty)
+                if pnl <= -0.02:
+                    new_res = pd.DataFrame({'ETF': [stock], 'CMP':[cmp], 'Amount': [amount], 'Qty': [qty], 'Down%':[round(pnl,2)]})
                     res = pd.concat([res,new_res],ignore_index=True)
             res_place.text('')
             res_place.table(res)
