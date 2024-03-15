6dd56c54122ab2f1319676a2c43d17b1454a0214
mridulgulati18 <mridulgulati18@gmail.com>
2024-03-15 17:13:14 +0530

    Updated Req

--- etf.py
+++ etf.py
@@ -29,7 +29,7 @@ if uploaded_file and st.button('Analyse'):
     st.session_state.button_pressed = True
 res_place = st.empty()
 while st.session_state.button_pressed:
-    res = pd.DataFrame(columns=['ETF', 'CMP', 'Amount', 'Qty', 'Down%'])
+    res = pd.DataFrame(columns=['ETF','Down%', 'CMP', 'Amount', 'Qty'])
     current_time = time.time()
     if current_time - st.session_state.last_analysis_time >= 300:
         st.session_state.last_analysis_time = current_time
@@ -48,7 +48,7 @@ while st.session_state.button_pressed:
                 qty = math.ceil(amount / cmp)
                 print(stock, cmp, pnl, amount, qty)
                 if pnl <= -0.02:
-                    new_res = pd.DataFrame({'ETF': [stock], 'CMP':[cmp], 'Amount': [amount], 'Qty': [qty], 'Down%':[round(pnl,2)]})
+                    new_res = pd.DataFrame({'ETF': [stock], 'Down%':[round(pnl,2)], 'CMP':[cmp], 'Amount': [amount], 'Qty': [qty]})
                     res = pd.concat([res,new_res],ignore_index=True)
             res_place.text('')
             res_place.table(res)
