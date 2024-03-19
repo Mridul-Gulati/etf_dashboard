46c8e1251bd00bb89c3c07ad323505d79667e260
mridulgulati18 <mridulgulati18@gmail.com>
2024-03-19 23:57:16 +0530

    First

--- etf.py
+++ etf.py
@@ -31,7 +31,7 @@ res_place = st.empty()
 total = 0
 total_place = st.empty()
 while st.session_state.button_pressed:
-    res = pd.DataFrame(columns=['ETF','Down%', 'CMP', 'Amount', 'Qty'])
+    res = pd.DataFrame(columns=['ETF','Down%', 'CMP', 'Amount', 'Qty', 'Last Buy'])
     current_time = time.time()
     if current_time - st.session_state.last_analysis_time >= 300:
         st.session_state.last_analysis_time = current_time
@@ -43,15 +43,17 @@ while st.session_state.button_pressed:
                 data = get_stock_data(f"{stock}.NS", today, today + timedelta(days=1))
                 cmp = round(data['Close'][-1],2)
                 buy_price = df.loc[df['ETF'] == stock, 'Buy Price'].values[0]
+                last_buy = df.loc[df['ETF'] == stock, 'LB'].values[0]
                 pnl = (cmp-buy_price)/buy_price
                 multi_fac = -1*round(pnl*1000,2)
                 variable = round((2500 * multi_fac)/100,2)
                 amount = int(2500 + variable)
                 qty = math.ceil(amount / cmp)
                 # print(stock, cmp, pnl, amount, qty)
-                if pnl <= -0.02:
-                    new_res = pd.DataFrame({'ETF': [stock], 'Down%':[round(pnl*100,2)], 'CMP':[cmp], 'Amount': [amount], 'Qty': [qty]})
-                    res = pd.concat([res,new_res],ignore_index=True)
+                if cmp < last_buy:
+                    if pnl <= -0.02:
+                        new_res = pd.DataFrame({'ETF': [stock], 'Down%':[round(pnl*100,2)], 'CMP':[cmp], 'Amount': [amount], 'Qty': [qty], 'Last Buy': [last_buy]})
+                        res = pd.concat([res,new_res],ignore_index=True)
             if res.empty:
                 total = 0
             else:
@@ -59,6 +61,6 @@ while st.session_state.button_pressed:
                 total_place.success('Total Amount: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + str(total))
 
             res_place.text('')
-            res_place.dataframe(res)
+            res_place.dataframe(res.sort_values('Down%'))
         else:
             st.warning('Please upload an Excel file')
