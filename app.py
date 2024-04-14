0631f84d77ad8992e25a1e21575133ff78bc4327
mridulgulati18 <mridulgulati18@gmail.com>
2024-04-14 20:35:02 +0530

    Added new ETF

--- app.py
+++ app.py
@@ -222,16 +222,6 @@ elif user == 'Hemank':
     st.session_state.all_data = all_data_h
     st.session_state.user = 'Hemank'
 
-if st.sidebar.button("Clear Cache"):
-    if st.session_state.user == 'Amit':
-        fetch_data_from_google_sheets.clear()
-    elif st.session_state.user == 'Deepti':
-        fetch_data_from_google_sheets_d.clear()
-    elif st.session_state.user == 'Mridul':
-        fetch_data_from_google_sheets_m.clear()
-    elif st.session_state.user == 'Hemank':
-        fetch_data_from_google_sheets_h.clear()
-    st.rerun()
 
 if 'total_invested' not in st.session_state:
     st.session_state.total_invested = 0
